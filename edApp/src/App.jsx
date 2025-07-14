823f2bc5572215e6ebc9dbbc236dc4f6033e7c68
iancorbett <iancorbett1324@gmail.com>
2025-07-14 16:37:16 -0700

    add routes to app

--- edApp/src/App.jsx
+++ edApp/src/App.jsx
@@ -2,6 +2,14 @@ import './App.css'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  )
   
 }
 
