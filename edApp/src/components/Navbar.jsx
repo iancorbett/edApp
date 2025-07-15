bce987dbe6636b233f42b08516a15264a93cbaee
iancorbett <iancorbett1324@gmail.com>
2025-07-14 18:04:52 -0700

    add link to navbar

--- edApp/src/components/Navbar.jsx
+++ edApp/src/components/Navbar.jsx
@@ -2,8 +2,16 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <nav>
-            hey
+        <nav className="fixed w-full z-40 transition-all duration-300">
+            <div className="container flex items-center justify-between">
+            <a className="text-xl font-bold text-primary flex items-center"
+            href="#"
+            >
+                <span className="relative z-10">
+                    <span className="text-glow text-foreground">ed</span>App
+                </span>
+            </a>
+            </div>
         </nav>
     )
     
