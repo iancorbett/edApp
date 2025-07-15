ee9a083d860fec587c7e39bc0fdda0eefd12cd40
iancorbett <iancorbett1324@gmail.com>
2025-07-14 19:00:30 -0700

    edit navbar

--- edApp/src/components/Navbar.jsx
+++ edApp/src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import { Link } from "react-router-dom";
+
 
 export const Navbar = () => {
     return (
-        <nav className="fixed w-full z-40 transition-all duration-300">
-            <div className="container flex items-center justify-between">
-            <a className="text-xl font-bold text-primary flex items-center"
-            href="#"
-            >
-                <span className="relative z-10">
-                    <span className="text-glow text-foreground">ed</span>App
-                </span>
+        <header className="shadow mb-2">
+            <div className="max-w-screen-lg mx-auto py-4 flex items-center">
+            <a href="#" className="text-2xl flex items-center font-black">
+                <span>edApp</span>
             </a>
+            <nav className="">
+                <ul className="flex md:gap-x-8">
+                    <li className=""><a href="">About</a></li>
+                    <li className=""><a href="">Sign Up</a></li>
+                    <li className=""><a href="" className="border-2 px-6 py-2 rounded-xl border-blue-600 text-blue-600 font-medium">Log In</a></li>
+                </ul>
+            </nav>
             </div>
-        </nav>
+        </header>
     )
-    
 }
