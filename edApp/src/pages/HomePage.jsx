d8b148652b12170fca3c2bfe2d0bbb6125c11d4a
iancorbett <iancorbett1324@gmail.com>
2025-07-18 22:18:49 -0700

    startbackground

--- edApp/src/pages/HomePage.jsx
+++ edApp/src/pages/HomePage.jsx
@@ -4,17 +4,14 @@ import { Navbar } from "../components/Navbar"
 
 export const HomePage = () => {
     return (
-        <>
+        <section className="bg-gradient-to-r from-blue-100 to-purple-100 px-6 py-16 lg:flex lg:items-center lg:justify-between">
             <Navbar />
+
             
-            <h1>EdApp</h1>
             
-            <Link to="/form">
-                <button className="bg-auto">
-                    Enter
-                </button>
-            </Link>
             
-        </>
+            
+            
+        </section>
     )
 }
