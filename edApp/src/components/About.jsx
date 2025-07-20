750c7b47570ddc333a291bc7836004e02f1923d4
iancorbett <iancorbett1324@gmail.com>
2025-07-19 22:56:18 -0700

    styling border

--- edApp/src/components/About.jsx
+++ edApp/src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const About = () => {
   return (
-    <section className="bg-gradient-to-r from-red-50 to-red-100 py-12 px-4 sm:px-6 border-2 border-amber-500">
+    <section className="bg-gradient-to-r from-red-50 to-red-100 py-12 px-4 sm:px-6 border-2 border-black rounded-md">
       <div className="max-w-5xl mx-auto text-center">
         <h2 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-6">
           About EdApp
