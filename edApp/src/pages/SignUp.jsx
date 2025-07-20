2ccacadf186fa541e8d14c398dc8a8793324f26c
iancorbett <iancorbett1324@gmail.com>
2025-07-19 23:24:45 -0700

    create signup page

---
+++ edApp/src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const SignUp = () => {
+    return (
+        <section>
+            <div>
+                signup section
+            </div>
+        </section>
+    )
+}
