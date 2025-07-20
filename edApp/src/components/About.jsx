94beb3a28f703b538c2e3ddc3fd009efd604823f
iancorbett <iancorbett1324@gmail.com>
2025-07-19 22:06:59 -0700

    add border-color

--- edApp/src/components/About.jsx
+++ edApp/src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const About = () => {
   return (
-    <section className="bg-white py-12 px-4 sm:px-6 border-2">
+    <section className="py-12 px-4 sm:px-6 border-2 border-amber-500">
       <div className="max-w-5xl mx-auto text-center">
         <h2 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-6">
           About EdApp
