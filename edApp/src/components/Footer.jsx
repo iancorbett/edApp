333bd167c46cb0c9917f7dc63e53d0240926573f
iancorbett <iancorbett1324@gmail.com>
2025-07-19 21:33:50 -0700

    style

--- edApp/src/components/Footer.jsx
+++ edApp/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className=" text-black text-center py-6">
+    <footer className=" text-black text-center py-12">
       <p className="text-sm">&copy; {new Date().getFullYear()} EdApp. All rights reserved.</p>
     </footer>
   );
