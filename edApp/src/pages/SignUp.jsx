a5e568d144ac48d93d4c93070921d0fdd9de175b
iancorbett <iancorbett1324@gmail.com>
2025-07-21 09:47:02 -0700

    add navbarto signup

--- edApp/src/pages/SignUp.jsx
+++ edApp/src/pages/SignUp.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { SignUpForm } from "../components/SignUpForm";
+import { Navbar } from "../components/Navbar";
 
 export const SignUp = () => {
     return (
         <section>
+            <Navbar />
             <div>
                 <SignUpForm />
             </div>
