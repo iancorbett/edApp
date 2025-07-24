a293db7b18250228cb0ddd8a145ec28247ec930f
iancorbett <iancorbett1324@gmail.com>
2025-07-23 18:08:53 -0700

    import students form

---
+++ edApp/src/components/AddStudentsForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const AddStudentsForm = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
--- edApp/src/pages/AddStudentsPage.jsx
+++ edApp/src/pages/AddStudentsPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
+import { AddStudentsForm } from "../components/AddStudentsForm";
 
 export const AddStudetsPage = () => {
     return (
         <section>
             <Navbar />
             <div>
-                
+                <AddStudentsForm />
             </div>
         </section>
     )
