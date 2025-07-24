e3cb2cfdb80172ea103030e839fab0e8d04112be
iancorbett <iancorbett1324@gmail.com>
2025-07-23 18:07:15 -0700

    addstudentspage

---
+++ edApp/src/pages/AddStudentsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+
+export const AddStudetsPage = () => {
+    return (
+        <section>
+            <Navbar />
+            <div>
+                
+            </div>
+        </section>
+    )
+}
