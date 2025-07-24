4b49c5a54252eb13e5b5f4fe4ed26c577ef23114
iancorbett <iancorbett1324@gmail.com>
2025-07-24 13:20:28 -0700

    add middleware to authenticate tokens when making a request

--- edApp/index.js
+++ edApp/index.js
@@ -68,4 +68,18 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
+  const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ error: "No token provided" });
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) return res.status(403).json({ error: "Invalid token" });
+
+    req.user = user; 
+    next();
+  });
+};
+
 app.listen(3001, () => console.log("Server running on http://localhost:3001"));
