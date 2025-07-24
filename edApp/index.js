4115ee3519844cb19ef35ee9aacb11d82e6cfb50
iancorbett <iancorbett1324@gmail.com>
2025-07-24 14:58:06 -0700

    testing backend with temporarry route

--- edApp/index.js
+++ edApp/index.js
@@ -101,4 +101,9 @@ app.post("/api/addstudent", authenticateToken, async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+
 app.listen(3001, () => console.log("Server running on http://localhost:3001"));
