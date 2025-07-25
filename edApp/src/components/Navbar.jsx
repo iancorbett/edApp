e433e4cee7729c038095fe6c22f74d41c7093114
iancorbett <iancorbett1324@gmail.com>
2025-07-25 15:21:31 -0700

    remove token upon logout and reirect user

--- edApp/src/components/Navbar.jsx
+++ edApp/src/components/Navbar.jsx
@@ -13,6 +13,12 @@ export const Navbar = () => {
     setIsLoggedIn(!!token);
   }, []);
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    navigate("/homepage"); 
+  };
+
   return (
     <nav className="fixed top-0 left-0 w-full bg-black shadow z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
