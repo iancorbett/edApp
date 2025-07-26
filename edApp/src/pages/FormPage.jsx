c35378ab99b36e6cdd058745a802e6aba1a4d4aa
iancorbett <iancorbett1324@gmail.com>
2025-07-26 11:31:57 -0700

    create formPage

---
+++ edApp/src/pages/FormPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+
+export const FormPage = () => {
+    return(
+        <section>
+            <Navbar />
+            <div>
+                <Form />
+            </div>
+        </section>
+    )
+}
