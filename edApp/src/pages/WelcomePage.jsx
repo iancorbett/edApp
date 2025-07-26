ea9fed56aed46a04c1a2c98830b4e87725bcab2a
iancorbett <iancorbett1324@gmail.com>
2025-07-26 11:27:29 -0700

    link button to form where input can be made

--- edApp/src/pages/WelcomePage.jsx
+++ edApp/src/pages/WelcomePage.jsx
@@ -23,7 +23,7 @@ export const WelcomePage = () => {
           📋 View Students
         </Link>
         <Link
-          to="/settings"
+          to="/form"
           className="block bg-yellow-500 text-white px-6 py-3 rounded hover:bg-yellow-600 transition"
         >
             <span>
