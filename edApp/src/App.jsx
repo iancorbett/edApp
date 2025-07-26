35566500d4b6d831f70f40612b74116732ced277
iancorbett <iancorbett1324@gmail.com>
2025-07-26 11:29:54 -0700

    move form into components file

--- edApp/src/App.jsx
+++ edApp/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
-import { Form } from './pages/Form'
+import { Form } from './components/Form'
 import { HomePage } from './pages/HomePage'
 import { About } from './components/About'
 import { SignUp } from './pages/SignUp'
