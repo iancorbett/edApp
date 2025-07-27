ec4941f4e2ccd7bec121040b5624ed24b641d699
iancorbett <iancorbett1324@gmail.com>
2025-07-26 22:26:11 -0700

    update middleware to support both admins and teachers using jwt

--- edApp/index.js
+++ edApp/index.js
@@ -93,10 +93,13 @@ app.post("/api/login", async (req, res) => {
 
   if (!token) return res.status(401).json({ error: "No token provided" });
 
-  jwt.verify(token, SECRET_KEY, (err, user) => {
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
     if (err) return res.status(403).json({ error: "Invalid token" });
 
-    req.user = user; 
+    req.user = {
+      id: decoded.user_id,
+      role: decoded.role,
+    }; 
     next();
   });
 };
