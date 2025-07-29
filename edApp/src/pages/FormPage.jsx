e9db2358caad65871df706d842daecbaf14f35b4
iancorbett <iancorbett1324@gmail.com>
2025-07-28 17:06:09 -0700

    add back button to form page

--- edApp/src/pages/FormPage.jsx
+++ edApp/src/pages/FormPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
 import { Form } from "../components/Form";
+import { BackButton } from "../components/BackButton";
 
 export const FormPage = () => {
     return(
         <section>
             <Navbar />
+            <BackButton />
             <div>
                 <Form />
             </div>
