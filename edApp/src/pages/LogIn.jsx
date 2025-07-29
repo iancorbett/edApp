7e8afc51188f9222215d92496a0de5b3d8843743
iancorbett <iancorbett1324@gmail.com>
2025-07-28 17:11:56 -0700

    add back button to login

--- edApp/src/pages/LogIn.jsx
+++ edApp/src/pages/LogIn.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { LogInForm } from "../components/LogInForm";
 import { Navbar } from "../components/Navbar";
+import { BackButton } from "../components/BackButton";
 
 export const LogIn = () => {
     return (
         <section>
             <Navbar />
+            <BackButton />
             <div>
                 <LogInForm />
             </div>
