570dd901001a908206733fe427d55638dbb671a1
iancorbett <iancorbett1324@gmail.com>
2025-07-28 17:04:14 -0700

    add back button to addstudents page

--- edApp/src/pages/AddStudentPage.jsx
+++ edApp/src/pages/AddStudentPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
 import { AddStudentForm } from "../components/AddStudentForm";
+import { BackButton } from "../components/BackButton";
 
 export const AddStudentPage = () => {
     return (
         <section>
             <Navbar />
+            <BackButton />
             <div>
                 <AddStudentForm />
             </div>
